c4acab5d5ff2e5480bc735bf54c3a2ba715ac09f
Rayan Adamczak <ag-hiro@hotmail.fr>
2020-05-19 17:34:29 +0200

    favicon & footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="footer">
-    Footer
+    <li>Plan du site</li>
+    <li>À propos</li>
+    <li>Nous contacter</li>
+    <li>Emploi</li>
+    <li>Publicité</li>
   </div>
 </template>
 
@@ -11,5 +15,17 @@
     width: 100vw;
     height: 140px;
     background: #EBEBE6;
+
+
+    text-align: center;
+  }
+  li {
+    margin:0 5vw 0 0;
+    top: 50%;
+    position: relative;
+    list-style: none;
+    display: inline;
+    text-align: center;
+    cursor: pointer;
   }
 </style>
