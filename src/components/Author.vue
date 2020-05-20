c076afedb432a1d657ed887b0764aba1b6c4be5c
Rayan Adamczak <ag-hiro@hotmail.fr>
2020-05-20 13:25:27 +0200

    date format correction

--- src/components/Author.vue
+++ src/components/Author.vue
@@ -7,7 +7,7 @@
     />
     <div class="author__infos">
       <h5 class="author__infos--name">Hayao Miyazaki</h5>
-      <h6 class="author__infos--date">May, 16 2020</h6>
+      <h6 class="author__infos--date">May 16, 2020</h6>
     </div>
   </div>
 </template>
